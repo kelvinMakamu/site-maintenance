1e24ddc48f295a28206f13c050e32f8a215f79c1
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-17 17:22:58 +0300

    Modified Database class to include PostgreSQL database credentials and connection string

---
+++ src/main/java/components/data/Database.java
@@ -0,0 +1,10 @@
+package components.data;
+
+import org.sql2o.Sql2o;
+
+public class Database {
+    private static String DB_URL   = "jdbc:postgresql://localhost:5432/site_maintenance";
+    private static String username = null;
+    private static String password = null;
+    public static Sql2o sql2o = new Sql2o(DB_URL,username,password);
+}
