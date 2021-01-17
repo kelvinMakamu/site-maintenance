129b83681b2eca89d98c3bdeebff9a5f8e09687e
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-17 17:05:57 +0300

    Modified App.java class to include getAssignedPort method, and defining the static file location for the application

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,16 @@
+import static spark.Spark.*;
+
 public class App {
 
-    public static void main(String[] args) {
+    static int getAssignedServerPort(){
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        return processBuilder.environment().get("PORT") != null
+                ? Integer.parseInt(processBuilder.environment().get("PORT"))
+                : 4500;
+    }
 
+    public static void main(String[] args) {
+        port(getAssignedServerPort());
+        staticFileLocation("/public");
     }
 }
