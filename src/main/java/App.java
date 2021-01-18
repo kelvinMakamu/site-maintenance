0904c6b3812c59ec02831e994f03b39357f285b5
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-18 10:03:19 +0300

    Modified App class to include POST routes for Site and Engineer

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -33,16 +33,48 @@ public class App {
             Map<String,Object> model  = new HashMap<>();
             List<Engineer> engineers = Engineer.all();
             model.put("engineers",engineers);
+            model.put("createdEngineer",req.session().attribute("createdEngineer"));
+            req.session().removeAttribute("createdEngineer");
+            model.put("updatedEngineer",req.session().attribute("updatedEngineer"));
+            req.session().removeAttribute("updatedEngineer");
+            model.put("deletedEngineer",req.session().attribute("deletedEngineer"));
+            req.session().removeAttribute("deletedEngineer");
             return new ModelAndView(model,"engineer.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/engineers",(req,res) -> {
+            Map<String,Object> model  = new HashMap<>();
+            String firstName  = req.queryParams("firstName");
+            String lastName   = req.queryParams("lastName");
+            Engineer engineer = new Engineer(firstName,lastName);
+            engineer.save();
+            req.session().attribute("createdEngineer","Engineer was added successfully!");
+            res.redirect("/engineers");
+            return null;
         }, new HandlebarsTemplateEngine());
 
         get("/sites",(req,res) -> {
             Map<String,Object> model  = new HashMap<>();
             List<Site> sites = Site.all();
             model.put("sites",sites);
+            model.put("createdSite",req.session().attribute("createdSite"));
+            req.session().removeAttribute("createdSite");
+            model.put("updatedSite",req.session().attribute("updatedSite"));
+            req.session().removeAttribute("updatedSite");
+            model.put("deletedSite",req.session().attribute("deletedSite"));
+            req.session().removeAttribute("deletedSite");
             return new ModelAndView(model,"site.hbs");
         }, new HandlebarsTemplateEngine());
 
-
+        post("/sites",(req,res) -> {
+            Map<String,Object> model  = new HashMap<>();
+            String name = req.queryParams("name");
+            String town = req.queryParams("town");
+            Site site   = new Site(name,town);
+            site.save();
+            req.session().attribute("createdSite","Site was added successfully!");
+            res.redirect("/sites");
+            return null;
+        }, new HandlebarsTemplateEngine());
     }
 }
